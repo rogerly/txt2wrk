fdbc676d902682afbf7f3eae1b134cd5d182c7e8
Roger Ly <rogerly@gmail.com>
2011-06-15 00:38:06 -0700

    Added registration url
    Registration gave a 404 because I was missing the urls.py update on the live server.

--- urls.py
+++ urls.py
@@ -5,6 +5,8 @@ from django.views.generic.simple import direct_to_template
 from django.contrib import admin
 admin.autodiscover()
 
+from registration.views import register
+
 urlpatterns = patterns('',
     # Example:
     # (r'^txt2wrk/', include('txt2wrk.foo.urls')),
@@ -19,4 +21,12 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    
+    url(r'^register/', 
+        register, 
+        { 
+         'backend': 'account.backends.ApplicantBackend',
+         'template_name': 'applicant/registration/registration_form.html', 
+        },
+        name='register'),
 )
