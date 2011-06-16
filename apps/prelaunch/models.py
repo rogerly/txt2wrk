5b59713d715b189b3d9e52fe67a3b6e675a34852
Jonathan Chan <jonchan726@gmail.com>
2011-06-15 23:52:13 -0700

    Updates for the splash page

--- apps/prelaunch/models.py
+++ apps/prelaunch/models.py
@@ -6,8 +6,9 @@ Created on Jun 13, 2011
 from django.db import models
 
 class PotentialUsers(models.Model):
-    first_name = models.CharField(max_length=20)
-    last_name = models.CharField(max_length=30)
-    email = models.CharField(max_length=30)
-    comments = models.CharField(max_length=100)
+    first_name = models.CharField(max_length=200,blank=True)
+    last_name = models.CharField(max_length=200,blank=True)
+    email = models.CharField(max_length=200)
+    comments = models.CharField(max_length=200,blank=True)
+    
     
