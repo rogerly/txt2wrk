951fd533861cec630e52e3a862ad7c7efda9a9c3
Roger Ly <rogerly@gmail.com>
2011-07-11 12:19:39 -0700

    Updated SMS messages
    Changed the SMS messages as spec'd by Dave and Alex.

--- apps/applicant/backends.py
+++ apps/applicant/backends.py
@@ -99,7 +99,7 @@ class ApplicantBackend(object):
         # send to Twilio.)
         sms = SMS.send(applicant=profile, 
                        phone_number=profile.mobile_number,
-                       message='Welcome to Txt2Wrk.  Please confirm your phone number by replying to this message.',
+                       message='Welcome to txt2wrk! To verify your phone number, reply with "OK". If you did not sign up with txt2wrk, reply with "STOP" to be removed from our system.',
                        message_type=REQ_NUMBER_CONFIRMATION)
 
         return auth_user
