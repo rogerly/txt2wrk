06f9f636f969e07e0bf278b42868ec39596abe63
Roger Ly <rogerly@gmail.com>
2011-07-11 18:09:07 -0700

    Splash page with login forms
    Added the dialog/tabbed login form

--- apps/prelaunch/views.py
+++ apps/prelaunch/views.py
@@ -7,6 +7,8 @@ Created on Jun 13, 2011
 from django.conf import settings
 from django.shortcuts import render_to_response
 from forms import PotentialUsersForm
+from applicant.forms import ApplicantLoginForm
+from employer.forms import EmployerLoginForm
 from django.template import RequestContext
 
 def contact(request):
@@ -25,4 +27,9 @@ def contact(request):
                 context_instance = RequestContext(request))
 
 def splash(request, template=None):
-    return render_to_response(template, { 'settings': settings }, context_instance = RequestContext(request))
+
+    ctxt = {}
+    ctxt['applicant_login_form'] = ApplicantLoginForm()
+    ctxt['employer_login_form'] = EmployerLoginForm()
+    ctxt['settings'] = settings
+    return render_to_response(template, ctxt, context_instance = RequestContext(request))
