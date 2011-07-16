fc77fdb1fb7da80be694a8a829e98cc5f79f7932
Roger Ly <rogerly@gmail.com>
2011-07-16 15:49:04 -0700

    Job assignments segregate demo/non-demo

--- apps/job_recommendation/views.py
+++ apps/job_recommendation/views.py
@@ -5,15 +5,19 @@ from django.template import RequestContext
 from forms import JobRecommendationForm
 
 def assign_job(request, template=None):
+    demo = False
+    if 'demo' in request.session:
+        demo = True
+
     if request.POST:
-        form = JobRecommendationForm(request.POST)
+        form = JobRecommendationForm(request.POST, demo=demo)
         ctxt = {}
         ctxt['form'] = form
         if form.is_valid():
             ctxt['success'] = True
-            ctxt['form'] = JobRecommendationForm()
+            ctxt['form'] = JobRecommendationForm(demo=demo)
             form.save()
 
         return render_to_response(template, ctxt, context_instance = RequestContext(request))
 
-    return render_to_response(template, {'form': JobRecommendationForm()}, context_instance = RequestContext(request))
+    return render_to_response(template, {'form': JobRecommendationForm(demo=demo)}, context_instance = RequestContext(request))
