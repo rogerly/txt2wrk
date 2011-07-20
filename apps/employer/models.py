bbf629fae1df53620b42c09c5c34522aa3c539a8
Roger Ly <rogerly@gmail.com>
2011-07-19 22:57:59 -0700

    Send email upon job application
    Added automated sending of email to employer when someone applies to their job.

--- apps/employer/models.py
+++ apps/employer/models.py
@@ -1,10 +1,17 @@
+import settings
+
+from smtplib import SMTP
+
 from django.core.urlresolvers import reverse
 from django.db import models
 from django.db.models.signals import post_save
+from django.dispatch import receiver
 from django.contrib.auth.models import User
+from django.template.loader import render_to_string
 from django.utils.translation import ugettext_lazy as _
 
 from account.models import Profile
+from applicant.signals import job_applied
 
 class EmployerProfile(Profile):
     phone_number = models.CharField('Phone Number',
@@ -62,3 +69,32 @@ class EmployerProfile(Profile):
             return reverse('employer_dashboard')
         else:
             return reverse('employer_profile')
+
+    # Contact employer upon job application
+    # Once applied, email should get sent to employer notifying them
+    # of a new job application
+    @staticmethod
+    @receiver(job_applied)
+    def send_email_notification(sender, **kwargs):
+        applicant = kwargs['applicant']
+        job = kwargs['job']
+        if applicant is not None and job is not None:
+            try:
+                employer = job.employer
+                ctxt = {}
+                ctxt['settings'] = settings
+                ctxt['job'] = job
+                ctxt['applicant'] = applicant
+                body = render_to_string('employer/email/notification_body.txt', ctxt)
+                m = SMTP(settings.EMAIL_HOST, settings.EMAIL_PORT)
+                m.ehlo()
+                if settings.EMAIL_USE_TLS:
+                    m.starttls()
+                    m.ehlo()
+                m.login(settings.EMAIL_HOST_USER, settings.EMAIL_HOST_PASSWORD)
+                m.sendmail(settings.EMAIL_HOST_USER,
+                           [employer.user.email],
+                           body)
+                m.close()
+            except:
+                pass
